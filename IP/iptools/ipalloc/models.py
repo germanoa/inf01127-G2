3aec0f5fa16df3644d875efd24d35f8e3f35da5f
Germano Andersson <germanoa@gmail.com>
2014-05-21 02:02:26 -0300

    init business rules at IP...

--- IP/iptools/ipalloc/models.py
+++ IP/iptools/ipalloc/models.py
@@ -6,6 +6,55 @@ class IP(models.Model):
     mask = models.IntegerField(default=0)
     version = models.IntegerField(default=4)
 
+    def __int_to_dottedIP(intip):
+        octet = ''
+        for exp in [3,2,1,0]:
+            octet = octet + str(intip / ( 256 ** exp )) + "." 
+            intip = intip % ( 256 ** exp )
+        return(octet.rstrip('.'))
+
+    def __dotted_to_intIP(dottedip):
+        octets = dottedip.split('.')
+        intip=0
+        for i in [3,2,1,0]:
+            intip = intip + int(octets[3-i]) * 256 ** i
+        return(intip)
+    
+    def __ip_to_net(ip,mask):
+        ipoctets = ip.split('.')
+        maskoctets = mask.split('.')
+        netip=0
+        for i in [3,2,1,0]:
+            ipoctet = (int(ipoctets[3-i]) * 256 ** i)
+            maskoctet = (int(maskoctets[3-i]) * 256 ** i)
+            netip = netip + (ipoctet & maskoctet)
+        return netip
+    
+    def set_address(self,dottedIP,version):
+        if version==4:
+            self.address = __dotted_to_intIP(dottedIP)
+
+    def get_address(self):
+        return __int_to_dottedIP(self.address)
+
+    def set_mask(self,dottedMASK,version):
+        if version==4:
+            self.mask = __dotted_to_intIP(dottedMASK)
+
+    def get_mask(self):
+        return __int_to_dottedIP(self.mask)
+
+    # t: {"int", "dotted"}
+    def get_net(self,t):
+        ip= self.get_address()
+        mask= self.get_mask()
+        if t=="int":
+            return __ip_to_net(ip,mask)
+        elif t=="dotted":
+            return __int_to_dotted(__ip_to_net(ip,mask))
+        else:
+            return -1
+
 class Network(IP):
     name = models.CharField(max_length=256)
     administrator = models.ForeignKey(User, null=True)
